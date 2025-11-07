f71e68f745d447efa2a35664fcf1621d7a50d76e
Dhafer-Khaoula <khaoula.dhafer@edu.univ-fcomte.fr>
2025-11-07 16:42:49 +0100

    deuxiemeexercice

--- src/js/script.js
+++ src/js/script.js
@@ -2,3 +2,9 @@
 const aMettreEnRouge = document.getElementById("a-mettre-en-rouge");
 
 aMettreEnRouge.style.color = "red";
+
+const enRougeSuiteAClick = document.querySelector("en-rouge-suite-a-click");
+
+enRougeSuiteAClick.addEventListener("click", () => {
+    enRougeSuiteAClick.style.color = "red"; 
+});

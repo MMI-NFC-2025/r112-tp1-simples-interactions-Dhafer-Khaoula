59967adb8f109192c02afff41cd412ad4b7690b7
Dhafer-Khaoula <khaoula.dhafer@edu.univ-fcomte.fr>
2025-11-07 17:03:59 +0100

    troisiemeexercice

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,21 @@
 // TODO
+//Exercice1
 const aMettreEnRouge = document.getElementById("a-mettre-en-rouge");
 
 aMettreEnRouge.style.color = "red";
 
+//Exercice2
 const enRougeSuiteAClick = document.querySelector("#en-rouge-suite-a-click");
 
 enRougeSuiteAClick.addEventListener("click", () => {
     enRougeSuiteAClick.style.color = "red"; 
 });
+
+//Exercice3
+const collH2 = document.querySelectorAll("h2");
+collH2.forEach((elm) => {
+    elm.addEventListener("click", (evt) => {
+
+evt.target.style.color = "red";
+    });
+  });
